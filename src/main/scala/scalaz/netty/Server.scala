992252d8ebf8dc37dd28924c3c34d2b02790df8b
Mianwo <shengcer@gmail.com>
2015-06-26 22:35:00 -0400

    eliminate val/var inside class (move them to constructor instead, interface of the factory method for these classes won't be changed though)

--- src/main/scala/scalaz/netty/Server.scala
+++ src/main/scala/scalaz/netty/Server.scala
@@ -34,13 +34,7 @@ import _root_.io.netty.channel.socket._
 import _root_.io.netty.channel.socket.nio._
 import _root_.io.netty.handler.codec._
 
-private[netty] class Server(bossGroup: NioEventLoopGroup, limit: Int) { server =>
-  // this isn't ugly or anything...
-  @volatile
-  private var channel: _root_.io.netty.channel.Channel = _
-
-  // represents incoming connections
-  private val queue = async.boundedQueue[(InetSocketAddress, Process[Task, Exchange[ByteVector, ByteVector]])](limit)
+private[netty] class Server(bossGroup: NioEventLoopGroup, channel: _root_.io.netty.channel.Channel, queue: async.mutable.Queue[(InetSocketAddress, Process[Task, Exchange[ByteVector, ByteVector]])]) { server =>
 
   def listen: Process[Task, (InetSocketAddress, Process[Task, Exchange[ByteVector, ByteVector]])] =
     queue.dequeue
@@ -55,74 +49,74 @@ private[netty] class Server(bossGroup: NioEventLoopGroup, limit: Int) { server =
       }
     } yield ()
   }
+}
 
-  private final class Handler(channel: SocketChannel)(implicit pool: ExecutorService) extends ChannelInboundHandlerAdapter {
+private[netty] final class ServerHandler(channel: SocketChannel, queue: async.mutable.Queue[ByteVector], serverQueue: async.mutable.Queue[(InetSocketAddress, Process[Task, Exchange[ByteVector, ByteVector]])])(implicit pool: ExecutorService) extends ChannelInboundHandlerAdapter {
 
-    // data from a single connection
-    private val queue = async.boundedQueue[ByteVector](limit)
+  // data from a single connection
+  //private val queue = async.boundedQueue[ByteVector](limit)
 
-    override def channelActive(ctx: ChannelHandlerContext): Unit = {
-      val process: Process[Task, Exchange[ByteVector, ByteVector]] =
-        Process(Exchange(read, write)) onComplete Process.eval(shutdown).drain
+  override def channelActive(ctx: ChannelHandlerContext): Unit = {
+    val process: Process[Task, Exchange[ByteVector, ByteVector]] =
+      Process(Exchange(read, write)) onComplete Process.eval(shutdown).drain
 
-      // gross...
-      val addr = channel.remoteAddress.asInstanceOf[InetSocketAddress]
+    // gross...
+    val addr = channel.remoteAddress.asInstanceOf[InetSocketAddress]
 
-      server.queue.enqueueOne((addr, process)).run
+    serverQueue.enqueueOne((addr, process)).run
 
-      super.channelActive(ctx)
+    super.channelActive(ctx)
+  }
+
+  override def channelInactive(ctx: ChannelHandlerContext): Unit = {
+    // if the connection is remotely closed, we need to clean things up on our side
+    queue.close.run
+
+    super.channelInactive(ctx)
+  }
+
+  override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit = {
+    val buf = msg.asInstanceOf[ByteBuf]
+    val dst = Array.ofDim[Byte](buf.capacity())
+    buf.getBytes(0, dst)
+    
+    val bv = ByteVector.view(dst)
+    
+    buf.release()
+
+    // because this is run and not runAsync, we have backpressure propagation
+    queue.enqueueOne(bv).run
+  }
+
+  override def exceptionCaught(ctx: ChannelHandlerContext, t: Throwable): Unit = {
+    queue.fail(t).run
+
+    // super.exceptionCaught(ctx, t)
+  }
+
+  // do not call more than once!
+  private def read: Process[Task, ByteVector] = queue.dequeue
+
+  private def write: Sink[Task, ByteVector] = {
+    def inner(bv: ByteVector): Task[Unit] = {
+      Task delay {
+        val data = bv.toArray
+        val buf = channel.alloc().buffer(data.length)
+        buf.writeBytes(data)
+
+        Netty toTask channel.writeAndFlush(buf)
+      } join
     }
 
-    override def channelInactive(ctx: ChannelHandlerContext): Unit = {
-      // if the connection is remotely closed, we need to clean things up on our side
-      queue.close.run
+    // TODO termination
+    Process constant (inner _)
+  }
 
-      super.channelInactive(ctx)
-    }
-
-    override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit = {
-      val buf = msg.asInstanceOf[ByteBuf]
-      val dst = Array.ofDim[Byte](buf.capacity())
-      buf.getBytes(0, dst)
-      
-      val bv = ByteVector.view(dst)
-      
-      buf.release()
-
-      // because this is run and not runAsync, we have backpressure propagation
-      queue.enqueueOne(bv).run
-    }
-
-    override def exceptionCaught(ctx: ChannelHandlerContext, t: Throwable): Unit = {
-      queue.fail(t).run
-
-      // super.exceptionCaught(ctx, t)
-    }
-
-    // do not call more than once!
-    private def read: Process[Task, ByteVector] = queue.dequeue
-
-    private def write: Sink[Task, ByteVector] = {
-      def inner(bv: ByteVector): Task[Unit] = {
-        Task delay {
-          val data = bv.toArray
-          val buf = channel.alloc().buffer(data.length)
-          buf.writeBytes(data)
-
-          Netty toTask channel.writeAndFlush(buf)
-        } join
-      }
-
-      // TODO termination
-      Process constant (inner _)
-    }
-
-    def shutdown: Task[Unit] = {
-      for {
-        _ <- Netty toTask channel.close()
-        _ <- queue.close
-      } yield ()
-    }
+  def shutdown: Task[Unit] = {
+    for {
+      _ <- Netty toTask channel.close()
+      _ <- queue.close
+    } yield ()
   }
 }
 
@@ -130,9 +124,12 @@ private[netty] object Server {
   def apply(bind: InetSocketAddress, config: ServerConfig)(implicit pool: ExecutorService): Task[Server] = Task delay {
     val bossGroup = new NioEventLoopGroup(config.numThreads)
 
-    val server = new Server(bossGroup, config.limit)
+    //val server = new Server(bossGroup, config.limit)
     val bootstrap = new ServerBootstrap
-
+    
+    val serverQueue = async.boundedQueue[(InetSocketAddress, Process[Task, Exchange[ByteVector, ByteVector]])](config.limit)
+    val handlerQueue = async.boundedQueue[ByteVector](config.limit)
+    
     bootstrap.group(bossGroup, Netty.workerGroup)
       .channel(classOf[NioServerSocketChannel])
       .childOption[java.lang.Boolean](ChannelOption.SO_KEEPALIVE, config.keepAlive)
@@ -144,7 +141,7 @@ private[netty] object Server {
               .addLast("frame decoding", new LengthFieldBasedFrameDecoder(Int.MaxValue, 0, 4, 0, 4))
           }
 
-          ch.pipeline.addLast("incoming handler", new server.Handler(ch))
+          ch.pipeline.addLast("incoming handler", new ServerHandler(ch, handlerQueue, serverQueue))
         }
       })
 
@@ -152,8 +149,8 @@ private[netty] object Server {
 
     for {
       _ <- Netty toTask bindF
-      _ <- Task delay {
-        server.channel = bindF.channel()      // yeah!  I <3 Netty
+      server <- Task delay {
+        new Server(bossGroup, bindF.channel(), serverQueue) // yeah!  I <3 Netty
       }
     } yield server
   } join
